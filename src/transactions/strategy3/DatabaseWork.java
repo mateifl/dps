f64202f12deef3e7d8a6602213153fb19e0b68ca
Matei Florescu <mateifl@yahoo.com>
2017-10-26 18:02:57 +0300

    .

--- src/transactions/strategy3/DatabaseWork.java
+++ src/transactions/strategy3/DatabaseWork.java
@@ -1,4 +1,4 @@
-package transactions.strategy2;
+package transactions.strategy3;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import dbaccess.beans.Customer;
 import dbaccess.beans.Order;
 
+
 /** General contract for a database unit of work that has to be executed in a transaction */
 public interface DatabaseWork<T> {
     void doInTransaction(T object);
@@ -14,6 +15,8 @@ public interface DatabaseWork<T> {
 
 
 abstract class AbstractDatabaseWork {
+	
+	protected DatabaseWork<Connection> worker;
 	
 	protected <T> void executePreparedStatement(Connection connection, String sql, ParameterMapper<T> mapper, T bean) 
 			throws SQLException {
@@ -24,20 +27,24 @@ abstract class AbstractDatabaseWork {
 	}
 }
 
-class InsertCustomer extends AbstractDatabaseWork implements DatabaseWork<Connection> {
+class UpdateCustomer extends AbstractDatabaseWork implements DatabaseWork<Connection> {
 
 	private ParameterMapper<Customer> customerMapper;
 	private Customer customer;
 	
-	public InsertCustomer(ParameterMapper<Customer> customerMapper, Customer customer) {
+	public UpdateCustomer(ParameterMapper<Customer> customerMapper, Customer customer, 
+						  DatabaseWork<Connection> worker) {
 		this.customerMapper = customerMapper;
 		this.customer = customer;
+		this.worker = worker;
 	}
 	
     @Override
     public void doInTransaction(Connection connection) {
         try {
-        	executePreparedStatement(connection, SQLStatements.INSERT_CUSTOMER, customerMapper, customer);
+        	if(this.worker != null)
+        		worker.doInTransaction(connection);
+        	executePreparedStatement(connection, SQLStatements.CUSTOMER_UPDATE, customerMapper, customer);
         }
         catch(SQLException e) {
             System.out.println(e.getMessage());
@@ -48,23 +55,20 @@ class InsertCustomer extends AbstractDatabaseWork implements DatabaseWork<Connec
 
 class InsertOrderWithCustomerUpdate extends AbstractDatabaseWork implements DatabaseWork<Connection> {
 
-	private ParameterMapper<Customer> customerMapper;
-	private Customer customer;
 	private ParameterMapper<Order> orderMapper;
 	private Order order;
 	
-	public InsertOrderWithCustomerUpdate(ParameterMapper<Customer> customerMapper, Customer customer,
-										 ParameterMapper<Order> orderMapper, Order order) {
-		this.customerMapper = customerMapper;
-		this.customer = customer;
+	public InsertOrderWithCustomerUpdate(ParameterMapper<Order> orderMapper, Order order, DatabaseWork<Connection> worker) {
 		this.orderMapper = orderMapper;
 		this.order = order;
+		this.worker = worker;
 	}
 	
 	@Override
 	public void doInTransaction(Connection connection) {
         try {
-        	executePreparedStatement(connection, SQLStatements.CUSTOMER_UPDATE, customerMapper, customer);
+        	if(worker != null)
+        		worker.doInTransaction(connection);
         	executePreparedStatement(connection, SQLStatements.INSERT_ORDER, orderMapper, order);
         }
         catch(SQLException e) {
