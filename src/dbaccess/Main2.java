63a4ca9b1d3a24721084f775431db768c046c9f2
mateifl <mateifl@yahoo.com>
2017-10-29 10:55:17 +0200

    .

--- src/dbaccess/Main2.java
+++ src/dbaccess/Main2.java
@@ -5,8 +5,7 @@ import utils.DatabaseBuilder;
 public class Main2 {
 
     public static void main(String[] args) {
-        DatabaseBuilder dbBuilder = new DatabaseBuilder();
-        dbBuilder.createDatabase();
+        DatabaseBuilder.createDatabase();
 
     }
 }
